3124d9b759b9b64dc9fb5e346df6f718b27c2eb6
Tommy Vedvik <tommyvedvik@gmail.com>
2025-02-18 23:19:07 +0400

    Update drawer.tsx

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -75,9 +75,9 @@ const DrawerContent = React.forwardRef<
   }, []);
 
   const styles = {
-    bottom: "fixed inset-x-2 bottom-2 flex h-auto max-h-[60vh] md:h-auto outline-none flex-col z-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom duration-300",
-    right: "fixed inset-x-2 md:left-auto md:inset-y-2 md:right-2 md:w-[340px] bottom-2 max-h-[60vh] md:max-h-[100vh] outline-none flex z-50 data-[state=open]:animate-in data-[state=closed]:animate-out md:data-[state=closed]:slide-out-to-right md:data-[state=open]:slide-in-from-right data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom duration-300",
-    left: "fixed inset-x-2 md:right-auto md:inset-y-2 md:left-2 md:w-[340px] bottom-2 max-h-[60vh] md:max-h-[100vh] outline-none flex z-50 data-[state=open]:animate-in data-[state=closed]:animate-out md:data-[state=closed]:slide-out-to-left md:data-[state=open]:slide-in-from-left data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom duration-300"
+    bottom: "fixed inset-x-2 bottom-2 flex h-auto max-h-[60vh] md:h-auto outline-none flex-col z-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom duration-600",
+    right: "fixed inset-x-2 md:left-auto md:inset-y-2 md:right-2 md:w-[340px] bottom-2 max-h-[60vh] md:max-h-[100vh] outline-none flex z-50 data-[state=open]:animate-in data-[state=closed]:animate-out md:data-[state=closed]:slide-out-to-right md:data-[state=open]:slide-in-from-right data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom duration-600",
+    left: "fixed inset-x-2 md:right-auto md:inset-y-2 md:left-2 md:w-[340px] bottom-2 max-h-[60vh] md:max-h-[100vh] outline-none flex z-50 data-[state=open]:animate-in data-[state=closed]:animate-out md:data-[state=closed]:slide-out-to-left md:data-[state=open]:slide-in-from-left data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom duration-600"
   };
 
   return (
