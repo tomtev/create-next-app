aad5425215a90cc9308c27cd4467f379bb043808
Tommy Vedvik <tommyvedvik@gmail.com>
2025-02-22 20:50:39 +0400

    ..

--- pages/[page]/locked/[link_id].tsx
+++ pages/[page]/locked/[link_id].tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { useToast } from '@/components/ui/use-toast';
+import { useToast } from '@/hooks/use-toast';
 
 const LockedPage: React.FC<{
   hasAccess: boolean;
